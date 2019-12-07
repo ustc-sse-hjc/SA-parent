c055627f786eaa7465016a14269698346a99b631
惠建诚 <929914571@qq.com>
2019-12-07 10:58:34 +0800

    add experiment requirement

---
+++ test.sql
@@ -0,0 +1,8 @@
+create table person(id varchar(255),name varchar(255),age int,sex int);
+ 
+insert into person values("SA19225025","Hui Jiancheng",23,1);
+insert into person values("SA19225027","Li Chuanguo",22,1);
+insert into person values("SA19225028","Li Chunguo",22,1);
+insert into person values("SA19225029","Li Jiayi",22,1);
+
+create table server(id varchar(255),cpu double(16,2) ,mem double(16,2) ,disk double(16,2));
